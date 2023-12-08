055a1033879e950b8068d3189fd0d3f5ed6629b6
RodrigoGimenez <rodrigogfxs@gmail.com>
2023-12-08 00:22:04 -0300

    email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Index from '@/components/Index'
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
     <main className="max-w-5xl m-auto overflow-hidden xl:overflow-visible">
       <Index />
-
+    <Footer />
     </main>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <footer className='w-full h-[50vh]'>
+        <section className='max-w-5xl px-4'>
+            <div>
+            <h2 className='text-white font-semibold text-2xl text-center'>Contactame</h2>
+                <form method="post">
+                    <div className='flex flex-col gap-3 text-white'>
+                        <label htmlFor="mail" className='pl-3'>Email</label>
+                        <input type="text" id='mail' className='bg-transparent border border-gray-500/50 pl-3 h-14 rounded-3xl outline outline-offset-0 outline-slate-700/40 placeholder:text-slate-400/50' placeholder='ejemplo@ejemplo.com' maxLength={90}/>
+
+                    </div>
+                </form>
+            </div>
+        </section>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
