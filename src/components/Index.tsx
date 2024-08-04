2abe86b4001275a39d08f96e52a7e3a8529dcd5d
AdolfKing <rodrigogfxs@gmail.com>
2024-08-04 18:37:32 -0300

    Update

--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -9,6 +9,7 @@ import { FaLinkedin, FaHtml5, FaLocationArrow } from "react-icons/fa";
 import { SiJavascript, SiTailwindcss, SiTypescript   } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
 import { TbBrandNextjs, TbTargetArrow  } from "react-icons/tb";
+import { IoLogoHtml5, IoLogoJavascript  } from "react-icons/io5";
 function Index() {
   const [hovered, setHovered] = useState(false);
   return (
@@ -194,7 +195,7 @@ function Index() {
 
                     <div className="flex bg-gray-800 md:px-3 items-center py-2 px-2 md:py-2 rounded-2xl gap-2">
                       <span className="text-sky-500 text-xl"> <SiTypescript    /> </span>
-                      <span className="uppercase font-medium text-sm">TypeScript</span>
+                      <span className="uppercase font-medium text-xs">TypeScript</span>
                     </div>
                   </div>
                 </div>
@@ -228,8 +229,39 @@ function Index() {
 
                     <div className="flex bg-gray-800 md:px-3 items-center py-2 px-2 md:py-2 rounded-2xl gap-2">
                       <span className="text-sky-500 text-xl"> <SiTypescript    /> </span>
-                      <span className="uppercase font-medium text-sm">TypeScript</span>
+                      <span className="uppercase font-medium text-xs">TypeScript</span>
                     </div>
+                  </div>
+                </div>
+              </Link>
+
+
+              {/* Fin card */}
+              {/* card */}
+
+
+              <Link href={"https://webui1.vercel.app/"} target="_blank" className="block relative group border-2 border-gray-700 hover:border-gray-300/30 rounded-xl shadow_translate-transition hover:sm:shadow sm:hover:-translate-y-1 text-gray-200 " onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}>
+    <span className={`text-gray-500 text-2xl  absolute right-4 top-3 transition-all ${
+      hovered ? 'block' : 'hidden'
+    }`}>
+    <TbTargetArrow  />
+    </span>
+                <div className="px-6 py-6">
+                  <h3 className="text-slate-200"> Web Ui</h3>
+                  <p className="text-slate-400">Clonacion de una pagina web</p>
+                  <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-4">
+                  <div className="flex bg-gray-800 md:px-3 items-center py-1 px-2 md:py-2 rounded-2xl gap-2">
+                      <span className="text-2xl md:text-2xl text-orange-600"> <IoLogoHtml5 /> </span>
+                      <span className="uppercase font-medium text-sm">HTML</span>
+                    </div>
+
+                    <div className="flex bg-gray-800 md:px-3 items-center py-1 px-2 md:py-2 rounded-2xl gap-2 ">
+                      <span className="text-yellow-500 text-lg md:text-2xl"> <IoLogoJavascript /> </span>
+                      <span className="uppercase font-medium text-xs">JavaScript</span>
+                    </div>
+
+                    
                   </div>
                 </div>
               </Link>
